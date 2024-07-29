9ac1aac9f8b26e6cf088a250f34e7c99463ec20d
nusud <buniakesteban@gmail.com>
2024-07-29 18:47:20 -0300

    Add templates and layout

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Esteban Buniak',
+        'title': 'First Blog Post',
+        'content': 'First post content',
+        'date_posted': 'July 29, 2024'
+    },
+    {
+        'author': 'Esteban Buniak',
+        'title': 'Second Blog Post',
+        'content': 'Second post content',
+        'date_posted': 'July 30, 2024'
+    }
+]
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
