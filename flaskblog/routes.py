360600099fd8996f1dba06a2e3420cbed3019bd7
nusud <buniakesteban@gmail.com>
2024-07-30 11:56:37 -0300

    Modifying project structure

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'b6b80645bb62a7f8b1a087989cb14d8e'
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
 
 posts = [
     {
@@ -50,6 +48,3 @@ def login():
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
-
-if __name__ == '__main__':
-    app.run(debug=True)
